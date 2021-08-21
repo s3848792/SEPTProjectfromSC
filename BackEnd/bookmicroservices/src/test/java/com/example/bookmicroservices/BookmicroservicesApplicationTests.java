c1b224ad63e59babb298734e7fc114d94b374bcc
[William Furler s3848792] <s3848792@student.rmit.edu.au>
2021-08-21 13:45:02 +1000

    Created bookmicroservice

---
+++ BackEnd/bookmicroservices/src/test/java/com/example/bookmicroservices/BookmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
