e56584b0f9ed9cd2398412bddf99d50b53385b90
JamesMurray3842096 <s3842096@student.rmit.edu.au>
2021-10-21 18:16:22 +1100

    Review UI updated with radio buttons for better interface. Boostrap updated in pom file

--- FrontEnd/myfirstapp/src/components/Reviews/LeaveReview.js
+++ FrontEnd/myfirstapp/src/components/Reviews/LeaveReview.js
@@ -16,7 +16,7 @@ class LeaveReview extends Component {
             bookID: book,
             userID: token.user,
             review: "",
-            rating: 0,
+            rating: 5,
             create_At: ""
         };
 
@@ -44,32 +44,63 @@ class LeaveReview extends Component {
         }
         this.props.addReview(newRev, this.props.history);
     }
+
+    oneIsSelected() {
+        this.state.rating=1;
+    }
+    twoSelected() {
+        this.state.rating=2;
+    }
+    threeSelected() {
+        this.state.rating=3;
+    }
+    fourSelected() {
+        this.state.rating=4;
+    }
+    fiveSelected() {
+        this.state.rating=5;
+    }
     render() {
         return (
             <div className="Book">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 m-auto">
-                            <h5 className="display-4 text-center">Create / Edit Book form</h5>
+                            <h5 className="display-4 text-center">Leave Review</h5>
                             <hr />
                             <form onSubmit={this.onSubmit}>
-                                <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg "
-                                           placeholder="Review"
-                                           name="review"
-                                           value= {this.state.review}
-                                           onChange = {this.onChange}
-                                    />
-                                </div>
 
+                                <div>
+                                    <h4>Star Rating</h4>
+                                <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                    <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" onChange={this.oneIsSelected.bind(this)}/>
+                                    <label className="btn btn-outline-primary" htmlFor="btnradio1">1</label>
+
+                                    <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" onChange={this.twoSelected.bind(this)}/>
+                                    <label className="btn btn-outline-primary" htmlFor="btnradio2">2</label>
+
+                                    <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" onChange={this.threeSelected.bind(this)}/>
+                                    <label className="btn btn-outline-primary" htmlFor="btnradio3">3</label>
+
+                                    <input type="radio" className="btn-check" name="btnradio" id="btnradio4" autoComplete="off" onChange={this.fourSelected.bind(this)}/>
+                                    <label className="btn btn-outline-primary" htmlFor="btnradio4">4</label>
+
+                                    <input type="radio" className="btn-check" name="btnradio" id="btnradio5" autoComplete="off" onChange={this.fiveSelected.bind(this)}/>
+                                    <label className="btn btn-outline-primary" htmlFor="btnradio5">5</label>
+
+                                </div>
+                                </div>
+                                <br/>
                                 <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg "
-                                           placeholder="Enter Rating (out of 5)"
-                                           name="rating"
-                                           value= {this.state.rating}
-                                           onChange = {this.onChange}
+                                    <textarea className="form-control form-control-lg"
+                                              placeholder="Review"
+                                              name="review"
+                                              value= {this.state.review}
+                                              onChange = {this.onChange}
                                     />
                                 </div>
+                                <br/>
+
                                 <input type="submit" className="btn btn-primary btn-block mt-4" />
                             </form>
                         </div>
